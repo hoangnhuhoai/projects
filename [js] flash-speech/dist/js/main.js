86f081b6ca38a8dd95137ca4ac7d81dc3656d82e
doraemon111 <hoangnhuhoai@gmail.com>
2022-07-07 17:03:58 +0700

    Added screenshot

--- [js] flash-speech/dist/js/main.js
+++ [js] flash-speech/dist/js/main.js
@@ -2,14 +2,14 @@
 const synth = window.speechSynthesis;
 
 // DOM Elements
-const textForm = document.querySelector('form');
-const textInput = document.querySelector('#text-input');
-const voiceSelect = document.querySelector('#voice-select');
-const rate = document.querySelector('#rate');
-const rateValue = document.querySelector('#rate-value');
-const pitch = document.querySelector('#pitch');
-const pitchValue = document.querySelector('#pitch-value');
-const body = document.querySelector('body');
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+const body = document.querySelector("body");
 
 // Init voices array
 let voices = [];
@@ -18,19 +18,18 @@ const getVoices = () => {
   voices = synth.getVoices();
 
   // Loop through voices and create an option for each one
-  voices.forEach(voice => {
+  voices.forEach((voice) => {
     // Create option element
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     // Fill option with voice and language
-    option.textContent = voice.name + '(' + voice.lang + ')';
+    option.textContent = voice.name + "(" + voice.lang + ")";
 
     // Set needed option attributes
-    option.setAttribute('data-lang', voice.lang);
-    option.setAttribute('data-name', voice.name);
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
     voiceSelect.appendChild(option);
   });
 };
-
 
 getVoices();
 if (synth.onvoiceschanged !== undefined) {
@@ -41,36 +40,35 @@ if (synth.onvoiceschanged !== undefined) {
 const speak = () => {
   // Check if speaking
   if (synth.speaking) {
-    console.error('Already speaking...');
+    console.error("Already speaking...");
     return;
   }
-  if (textInput.value !== '') {
-    
+  if (textInput.value !== "") {
     // Add background animation
     body.style.backgroundImage = "url('img/bg.png')";
-    body.style.backgroundRepeat = 'repeat-x';
-    body.style.backgroundSize = '100% 100%';
-    
-  
+    body.style.backgroundRepeat = "repeat-x";
+    body.style.backgroundSize = "100% 100%";
+
     // Get speak text
     const speakText = new SpeechSynthesisUtterance(textInput.value);
 
     // Speak end
-    speakText.onend = e => {
-      console.log('Done speaking...');
+    speakText.onend = (e) => {
+      console.log("Done speaking...");
       body.style.backgroundImage = "url('img/bg.png')";
     };
 
     // Speak error
-    speakText.onerror = e => {
-      console.error('Something went wrong');
+    speakText.onerror = (e) => {
+      console.error("Something went wrong");
     };
 
     // Selected voice
-    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    const selectedVoice =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
 
     // Loop through voices
-    voices.forEach(voice => {
+    voices.forEach((voice) => {
       if (voice.name === selectedVoice) {
         speakText.voice = voice;
       }
@@ -87,17 +85,17 @@ const speak = () => {
 // EVENT LISTENERS
 
 // Text form submit
-textForm.addEventListener('submit', e => {
+textForm.addEventListener("submit", (e) => {
   e.preventDefault();
   speak();
   textInput.blur();
 });
 
 // Rate value change
-rate.addEventListener('change', e => (rateValue.textContent = rate.value));
+rate.addEventListener("change", (e) => (rateValue.textContent = rate.value));
 
 // Pitch value change
-pitch.addEventListener('change', e => (pitchValue.textContent = pitch.value));
+pitch.addEventListener("change", (e) => (pitchValue.textContent = pitch.value));
 
 // Voice select change
-voiceSelect.addEventListener('change', e => speak());
+voiceSelect.addEventListener("change", (e) => speak());
